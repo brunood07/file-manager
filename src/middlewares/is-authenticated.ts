b533cccf205e951acbcecf70ca396dd0fa303d3d
brunood07 <brunood07@gmail.com>
2023-11-04 16:49:40 -0300

    feat: isAuthenticated middleware created

---
+++ src/middlewares/is-authenticated.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+    sub: string;
+}
+
+export async function isAuthenticated(request: Request, response: Response, next: NextFunction) {
+
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        throw new Error("Token missing!");
+    }
+
+    const [, token] = authHeader.split(" ");
+
+    try {
+        const { sub: user_id } = verify(token, String(process.env.CLIENT_SECRET_KEY)) as IPayload;
+
+        request.user = {
+            id: user_id
+        }
+
+        next();
+    } catch {
+        throw new Error("Invalid token!");
+    }
+}
